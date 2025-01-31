387b56ef69626d715fad9e53fb189aac82a4a747
Jennica1 <jsask1@my.bcit.ca>
2025-01-31 13:42:28 -0800

    added fade animations

--- .history/src/app/pocket-pals/page_20250131133750.js
+++ .history/src/app/pocket-pals/page_20250131133750.js
@@ -9,7 +9,7 @@ export default function pocketPals() {
   return (
     <div className="flex justify-center flex-col items-center">
       <Header />
-      <main className="lg:p-10 p-5 opacity-0 animate-fadeIn animate-slideIn">
+      <main className="lg:p-10 p-5 opacity-0 animate-fadeIn">
         <AppInformation />
         <Information className="" />
 
--- .history/tailwind.config_20250131133606.mjs
+++ .history/tailwind.config_20250131133606.mjs
@@ -19,16 +19,10 @@ export default {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
-        slideIn: {
-          "0%": { opacity: "0", transform: "translateY(20px)" },
-          "100%": { opacity: "1", transform: "translateY(0)" },
-        },
       },
       animation: {
         fadeIn: "fadeIn 1s ease-out forwards",
-        slideIn: "slideIn 1s ease-out forwards",
       },
-      
     },
   },
   plugins: [],
