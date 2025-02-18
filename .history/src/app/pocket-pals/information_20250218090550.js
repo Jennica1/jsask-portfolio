ddeaee78523c83f3ad74a80aa7afcdfaf158f871
Jennica1 <jsask1@my.bcit.ca>
2025-02-18 10:27:29 -0800

    so many

---
+++ .history/src/app/pocket-pals/information_20250218090550.js
@@ -0,0 +1,41 @@
+import Slideshow from "../components/Slideshow/Slideshow"
+import Image from "next/image"
+
+export default function Information() {
+    return(
+        <>
+        <section className="flex flex-row justify-center gap-6 w-full max-w-6xl mx-auto pt-10 mt-20">
+          {/* Left Box */}
+          <div className="w-[8px] bg-[#890620] h-grow"></div>
+
+          {/* Main Content */}
+          <div className="flex-1 max-w-fit">
+            <h2 className="md:mt-3 text-xl font-bold">Context:</h2>
+            <p className="max-w-xl">
+              Caregivers face significant challenges with repetitive, stressful, and time-consuming paperwork which affects their ability to focus on providing quality care to seniors. Although there are tools that assist in form filling, there hasn't any specifically targeted towards caregivers.
+            </p>
+
+            <h2 className="mt-6 md:mt-3 text-xl font-bold">
+              Key Issues to Solve:
+            </h2>
+            <ul className="list-disc ml-6 mb-6">
+              <li>
+                Relieving caregivers of tedious tasks of repetitive form
+                filling.
+              </li>
+              <li>
+                Translating document jargon to more easy-to-understand terms.
+              </li>
+              <li>
+                Unnecessary effort to look for different Canadian government
+                forms.
+              </li>
+              <li>
+                Managing several client information and keeping it organized.
+              </li>
+            </ul>
+          </div>
+        </section>
+        </>
+    )
+}
---
+++ .history/src/app/pocket-pals/information_20250218091108.js
@@ -0,0 +1,51 @@
+import Slideshow from "../components/Slideshow/Slideshow";
+import Image from "next/image";
+
+export default function Information() {
+  return (
+    <>
+      <section className="flex flex-row justify-center gap-6 w-full max-w-6xl mx-auto pt-10 mt-20">
+        {/* Left Box */}
+        <div className="w-[8px] bg-[#890620] h-grow"></div>
+
+        {/* Main Content */}
+        <div className="flex-1 max-w-fit">
+          <h2 className="md:mt-3 text-xl font-bold">Background:</h2>
+          <p className="max-w-xl">
+            The goal was to create an app that educated children about
+            endangered animals world issue in order to develop an early
+            understanding on what they can do to help or be mindful of.
+          </p>
+
+          <h2 className="mt-6 md:mt-3 text-xl font-bold">Key Features:</h2>
+          <ul className="list-disc ml-6 mb-6">
+            <li>
+              Basic language as our target demographic is children ages 10-12.
+            </li>
+            <li>Simple interface to reduce confusion when navigating.</li>
+            <li>
+              Implemented colours that elementary-aged children commonly engage
+              positively with.
+            </li>
+            <li>
+              Games that are simple enough for children but makes learning more
+              engaging.
+            </li>
+          </ul>
+
+          <h2 className="md:mt-3 text-xl font-bold">Challenges Faced:</h2>
+          <ul className="list-disc ml-6 mb-6">
+            <li>
+              Colour choices were bright but, visually unappealing and
+              frequently clashed together.
+            </li>
+            <li>
+              Balancing the amount of article information that is informative
+              but can still be understood by young children.{" "}
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+}
